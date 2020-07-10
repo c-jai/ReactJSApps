48626d4ec63c224fee378f10d77f6b561aeeb95d
Jai <jai.chhatre@gmail.com>
2020-07-09 19:17:06 -0700

    Updates

---
+++ precache-manifest.22824ce2196efc8513d0751f7b1b92ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8533894576bf817fb0179d5188a589dd",
+    "url": "/ReactJSApps/index.html"
+  },
+  {
+    "revision": "53dd3d584324776921c8",
+    "url": "/ReactJSApps/static/css/main.a110cea9.chunk.css"
+  },
+  {
+    "revision": "1c6993ecdf592cc342a6",
+    "url": "/ReactJSApps/static/js/2.834ff4e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJSApps/static/js/2.834ff4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53dd3d584324776921c8",
+    "url": "/ReactJSApps/static/js/main.ded1e08d.chunk.js"
+  },
+  {
+    "revision": "188500b50fcf3558b37e",
+    "url": "/ReactJSApps/static/js/runtime-main.f0d07678.js"
+  }
+]);
